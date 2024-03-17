0173f6883200862b6426eba614d4e62646f46eb4
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-03-17 13:27:37 +0900

    [fix] 배경 추가

--- src/components/layout/MainLayout.vue
+++ src/components/layout/MainLayout.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="login-bg">
-      <div class="login-wrap">
+    <div class="main-bg">
+      <div class="main-wrap">
         <div>
           <!-- /// 이미지(+svg) 테스트 -->
           <img src="@/assets/logo.svg" alt="logo"/>
           <div class="text-center">
             hyeok's bakery!
           </div>
-      
         </div>
-        <div class="clear"></div>
       </div>
     </div>
   </template>
