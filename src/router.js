1f09c93a7ac22c839cff4d880ffd77be3d6c1494
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-03-17 13:06:38 +0900

    [fix] 필요없는 부분 삭제

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ const routes = [
   {
     /// 화면에 표시되는 url
     path: "/",
-    name: "main",
+    name: "Main",
     /// 컴포넌트 호출 시 아래와 같이 호출해야 각 페이지마다 호출이기 때문에 로딩 속도가 빠름
     component: () => import("@/components/layout/MainLayout"),
   },
