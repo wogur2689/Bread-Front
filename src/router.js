e4d7943d97ffb9f27852411aed08a8d9f8066262
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-05-24 16:54:11 +0900

    fix : gnb css수정

--- src/router.js
+++ src/router.js
@@ -4,11 +4,9 @@ import MainLayout from "@/components/layout/MainLayout";
 
 const routes = [
   {
-    /// 화면에 표시되는 url
     path: "/",
     name: "Main",
     component: MainLayout,
-    /// 컴포넌트 호출 시 아래와 같이 호출해야 각 페이지마다 호출이기 때문에 로딩 속도가 빠름
   },
   {
     /// 화면에 표시되는 url
@@ -18,11 +16,9 @@ const routes = [
     component: () => import("@/components/layout/LoginLayout")
   },
   {
-    /// 화면에 표시되는 url
     path: "/signUp",
     name: "signUp",
-    /// 컴포넌트 호출 시 아래와 같이 호출해야 각 페이지마다 호출이기 때문에 로딩 속도가 빠름
-    component: () => import("@/components/layout/signUpLayout")
+    component: () => import("@/components/layout/SignUpLayout")
   },
   {
     path: "/Product",
@@ -36,19 +32,6 @@ const routes = [
       },
     ],
   },
-  // {
-  //   path: "/recruit",
-  //   name: "MainRecruit",
-  //   component: MainLayout,
-  //   children: [
-  //     { path: "", redirect: "/recruit/manage" },
-  //     { path: "main", component: () => import("@/components/menu/RecruitGnb") },
-  //     {
-  //       path: "manage",
-  //       component: () => import("@/components/recruit/ManageLnb"),
-  //     },
-  //   ],
-  // },
   {
     /// 404 에러
     path: "/:pathMatch(.*)",
