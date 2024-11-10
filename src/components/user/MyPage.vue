1389fbfe6a312976b07aa648a81819027ea2c683
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-11-10 23:43:54 +0900

    fix : mypage api binding

--- src/components/user/MyPage.vue
+++ src/components/user/MyPage.vue
@@ -4,12 +4,12 @@
             <h1>마이페이지</h1>
             <hr>
             <form method="post">
-                <div class="hint">아이디 :</div><input type="text" ref="userId" placeholder="아이디"/>
-                <div class="hint">이름 :</div><input type="text" ref="name" placeholder="이름"/>
-                <div class="hint">비밀번호 :</div><input type="password" ref="pwd" placeholder="비밀번호"/>
-                <div class="hint">나이 :</div><input type="text" ref="age" placeholder="나이"/>
-                <div class="hint">핸드폰번호 :</div><input type="text" ref="phone" placeholder="ex)010-0000-0000"/>
-                <div class="hint">주소 :</div><input type="text" ref="address" placeholder="주소"/>
+                <div class="hint">아이디 :</div><input type="text" ref="userId" placeholder="아이디" v-bind:value="usr.userId" />
+                <div class="hint">이름 :</div><input type="text" ref="name" placeholder="이름" v-bind:value="usr.name"/>
+                <div class="hint">비밀번호 :</div><input type="password" ref="pwd" placeholder="비밀번호" v-bind:value="usr.pwd"/>
+                <div class="hint">나이 :</div><input type="text" ref="age" placeholder="나이" v-bind:value="usr.age"/>
+                <div class="hint">핸드폰번호 :</div><input type="text" ref="phone" placeholder="ex)010-0000-0000" v-bind:value="usr.phone"/>
+                <div class="hint">주소 :</div><input type="text" ref="address" placeholder="주소" v-bind:value="usr.address"/>
                 <div class="su">
                     <button type="button" class="btn" v-on:click="myPageUpdate">수정하기</button>
                 </div>
@@ -26,6 +26,7 @@ export default {
     data() {
         return {
             usr: {
+                userId: '',
                 name: '',
                 pwd: '',
                 age: '',
@@ -45,7 +46,8 @@ export default {
         };
     },
     mounted() {
-
+        //페이지가 랜더링 된 후 API 호출
+        this.getMyPageData();
     },
     methods: {
         // error message alert
@@ -57,7 +59,17 @@ export default {
             return false;
         },
 
-        // 회원가입
+        async getMyPageData() {
+            const res = await this.$axios.post('http://localhost:3000/api/myPageUpdate', this.usr);
+            if (res.data.code == '0000') {
+                // 정상일 경우
+                this.usr = res.data.info;
+            } else {
+                this.usr = null;
+            }
+        },
+
+        // 마이페이지 수정하기
         async myPageUpdate() {
             try {
             // 아이디 빈칸 & 비밀번호 빈칸 체크
@@ -76,13 +88,12 @@ export default {
             errTxt = vd.pwdMatch(this.usr.address);
             if (this.isErrTxt(errTxt)) return;
             
-            const res = await this.$axios.post('http://localhost:3000/myPage/Update', this.usr);
+            const res = await this.$axios.post('http://localhost:3000/api/myPageUpdate', this.usr);
             if (res.data.code == '0000') {
-                /// 정상일 경우 로그인으로 이동
+                // 정상
                 this.usr = res.data.info;
-                this.$router.push('/login');
             } else {
-                /// 그 외
+                // 그 외
                 this.$refs.id.focus();
                 alert(res.data.msg);
                 return;
