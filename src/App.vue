aff42d389453dfed961981409da11a1f56e7e37f
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-12-21 16:15:03 +0900

    renewal : 스택 변경 vue -> next.js

--- src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <!-- /// :key로 변경된 url 경로 인식 -->
-    <router-view :key="$route.fullPath"></router-view>
-  </div>
-</template>
--- src/components/layout/NotFoundComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="error-page">
-      <h1>404</h1>
-      <p>Page Not Found</p>
-      <div class="errorImg"></div>
-      <a href="/">Go Home</a>
-    </div>
-</template>
