67665fe7e4fc208f5a5aa6bc2238a1b5fd87e4a7
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2025-07-23 01:23:03 +0900

    fix : 타 페이지 메뉴 표출 안되는 오류 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import Layout from '@/components/common/Layout';
 import '../styles/globals.css'; // TailwindCSS 스타일
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { Menu } from '@/types/web/menu';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   // menuData를 모든 페이지로 전달 (SSR 또는 GSP에서 받아온 경우)
-    const { menuData } = pageProps;
+    const [menuData, setMenuData] = useState<Menu[]>([]);
+
+    useEffect(() => {
+        const stored = sessionStorage.getItem('menuData');
+        if (stored) {
+            setMenuData(JSON.parse(stored));
+        } else {
+            fetch('http://localhost:3001/menu/list')
+                .then(res => res.json())
+                .then(data => {
+                    sessionStorage.setItem('menuData', JSON.stringify(data.data));
+                    setMenuData(data.data);
+                });
+        }
+    }, []);
 
     return (
         <Layout menuData={menuData || []}>
